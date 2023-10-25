ee36788eb6160a243fca923399315a36c1f8184f
neville777 <neville.james@student.moringaschool.com>
2023-10-25 08:22:09 +0300

    react-app

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,16 @@
 import React from "react";
-import blogData from "../data/blog";
 import Header from "./Header";
 import About from "./About";
 import ArticleList from "./ArticleList";
-import Article from "./Article.js";
+import data from'../data/blog';
 
-console.log(blogData);
 
 function App() {
   return (
     <div className="App">
+      <Header name={data.name} />
+      <About image={data.image} about={data.about} />
+      <ArticleList posts={data.posts} />
     </div>
   );
 }
